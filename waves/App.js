ee2df6e0803c83567ec72a0048104c26059afdb4
GhostNickbr <65207883+GhostNickbr@users.noreply.github.com>
2021-10-24 22:57:41 -0300

    Base

---
+++ waves/App.js
@@ -0,0 +1,30 @@
+import { Button } from '@mui/material';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Globais from './Globais';
+import Requisicao from './Requisicao'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        onPress={onPressLearnMore}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
